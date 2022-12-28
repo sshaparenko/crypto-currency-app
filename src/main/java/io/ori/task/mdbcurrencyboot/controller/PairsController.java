fac59109b4e17527d5afb2fbc376bdf0584fb02d
sshaparenko <shaparenko.archicgi@gmail.com>
2022-12-28 16:38:02 +0200

    - add controllers for HttpUrlConnection and Async HttpClient request methods - add HttpUrlConnection and Async HttpClient request methods

--- src/main/java/io/ori/task/mdbcurrencyboot/controller/PairsController.java
+++ src/main/java/io/ori/task/mdbcurrencyboot/controller/PairsController.java
@@ -8,16 +8,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import reactor.core.publisher.Mono;
 
 
 @Controller
 public class PairsController {
     @Autowired
     private PairsService pairsService;
+
     //private static final Logger logger = LoggerFactory.getLogger(PairsController.class);
     @GetMapping("/pairs/{pair1}/{pair2}")
-    public Pairs getPairsCurrency(@PathVariable String pair1, @PathVariable String pair2) {
-        return pairsService.getCurrencyTextJson(pair1, pair2);
+    public Pairs getPairsRestTemplate(@PathVariable String pair1, @PathVariable String pair2) {
+        return pairsService.getPairsRestTemplate(pair1, pair2);
     }
+
+    @GetMapping("pairs/httpurlconnection/{pair1}/{pair2}")
+    public Pairs getPairsHttpUrlConnection(@PathVariable String pair1, @PathVariable String pair2) {
+        return pairsService.getPairsHttpUrlConnection(pair1, pair2);
+    }
+
+    @GetMapping("/pairs/httpclient/async/{pair1}/{pair2}")
+    public Pairs getPairsHttpClientAsync(@PathVariable String pair1, @PathVariable String pair2) {
+        return pairsService.getPairsHttpClientAsync(pair1, pair2);
+    }
+
+//    @GetMapping("/pairs/webclient/{pair1}/{pair2}")
+//    public Pairs getMonoPairsWebClient(@PathVariable String pair1, @PathVariable String pair2) {
+//        return pairsService.getMonoPairsWebClient(pair1, pair2);
+//    }
 }
--- src/main/java/io/ori/task/mdbcurrencyboot/service/PairsService.java
+++ src/main/java/io/ori/task/mdbcurrencyboot/service/PairsService.java
@@ -11,8 +11,21 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 
 @Service
@@ -20,16 +33,17 @@ public class PairsService {
 
     @Autowired
     private Pairs pairs;
-
     private static final Logger logger = LoggerFactory.getLogger(PairsService.class);
-
     private final RestTemplate restTemplate;
+    private final WebClient webClient;
+    private URL getPairsHttpUrl;
 
-    public PairsService(RestTemplateBuilder restTemplateBuilder) {
+    public PairsService(RestTemplateBuilder restTemplateBuilder, WebClient.Builder webClientBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+        this.webClient = webClientBuilder.baseUrl("https://cex.io/api/").build();
     }
 
-    public Pairs  getCurrencyTextJson(String pair1, String pair2) {
+    public Pairs getPairsRestTemplate(String pair1, String pair2) {
         String url = "https://cex.io/api/ticker/{pair1}/{pair2}";
         String response = restTemplate.getForObject(url, String.class, pair1, pair2);
         try {
@@ -43,16 +57,77 @@ public class PairsService {
         return pairs;
     }
 
-    public ResponseEntity<Pairs>  getCurrencyPlainJson(String pair1, String pair2) {
-        String url = "https://cex.io/api/ticker/{pair1}/{pair2}";
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-        HttpEntity<Pairs> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(url, HttpMethod.GET, entity, Pairs.class, pair1, pair2);
-//        return this.restTemplate.getForObject(url, Pairs.class, pair1, pair2);
+    public Pairs getPairsHttpUrlConnection(String pair1, String pair2) {
+        try {
+            getPairsHttpUrl = new URL("https://cex.io/api/ticker/" + pair1 + "/" + pair2);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            HttpURLConnection connection = (HttpURLConnection) getPairsHttpUrl.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            pairs = new ObjectMapper().readValue(inputStream, Pairs.class);
+        } catch (IOException e) {
+            logger.info(e.getMessage());
+        }
+        logger.info("Pairs object: " + pairs);
+        return pairs;
     }
+
+    public Pairs getPairsHttpClientAsync(String pair1, String pair2) {
+        var client = HttpClient.newHttpClient();
+
+        var request = HttpRequest.newBuilder(
+                URI.create("https://cex.io/api/ticker/" + pair1 + "/" + pair2))
+                .header("accept", "application/json")
+                .GET()
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        try {
+            String stringResponse = response.get().body();
+            try {
+                pairs = new ObjectMapper().readValue(stringResponse, Pairs.class);
+            } catch (JsonMappingException e) {
+                logger.info("Json mapper exception: " + e.getMessage());
+            } catch (JsonProcessingException e) {
+                logger.info("Json processing exception" + e.getMessage());
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info("Pairs object: " + pairs);
+        return pairs;
+    }
+
+    //trying to use WebClient to send HTTP request
+
+//    public Pairs getMonoPairsWebClient(String pair1, String pair2) {
+//        String url = "https://cex.io/api/ticker/{pair1}/{pair2}";
+//        String result = " ";
+//
+//        Mono<String> monoPairs = this.webClient.get()
+//                .uri("/ticker/{pair1}/{pair2}", pair1, pair2)
+//                .retrieve().bodyToMono(String.class);
+//
+//        monoPairs.subscribe(
+//                value -> result,
+//                error -> error.printStackTrace(),
+//                () -> logger.info("Completed without a value")
+//        );
+//
+//        logger.info("Mono Pairs: " + monoPairs);
+//
+//        try {
+//            pairs = new ObjectMapper().readValue(monoPairs, Pairs.class);
+//        } catch (JsonMappingException e) {
+//            logger.info("Json mapper exception: " + e.getMessage());
+//        } catch (JsonProcessingException e) {
+//            logger.info("Json processing exception" + e.getMessage());
+//        }
+//        return pairs;
+//    }
 }
